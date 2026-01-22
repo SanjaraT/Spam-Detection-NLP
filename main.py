fef43cd2ffdaa146aa16def75d9cf0041b82b10c
sanjaraT <sanjaratunola@gmail.com>
2026-01-22 17:45:34 +0600

    Naive Bayes

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import nltk
 nltk.download ('stopwords')
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, confusion_matrix
 
 
 df = pd.read_csv("spam.csv",encoding='latin-1')
@@ -35,3 +38,15 @@ tfidf = TfidfVectorizer(
 )
 X = tfidf.fit_transform(df['text'])
 y = df['label']
+
+#Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X,y, test_size=0.20, random_state = 42
+)
+
+#Naive Bayes
+NB = MultinomialNB()
+NB.fit(X_train,y_train)
+
+pred_nb = NB.predict(X_test)
+print("Naive Bayes\n",classification_report(y_test, pred_nb))
