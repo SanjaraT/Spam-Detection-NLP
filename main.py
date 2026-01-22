c4f78de9089e0c29aef6e3bde48871f01fe0d793
sanjaraT <sanjaratunola@gmail.com>
2026-01-22 17:25:53 +0600

    Preprocessing

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import re
+import nltk
+nltk.download ('stopwords')
+from nltk.corpus import stopwords
 
 
 df = pd.read_csv("spam.csv",encoding='latin-1')
 df = df[['v1', 'v2']]
 df.columns = ['label', 'text']
+# print(df.head())
+# print(df['label'].value_counts())
+
+#Label Encoding
+le = LabelEncoder()
+df['label'] = le.fit_transform(df['label'])
+
+#Text Preprocessing
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\d+','',text)
+    text = re.sub(r'[^\w\s]','',text)
+    text = text.strip()
+    return text
+df['text']=df['text'].apply(clean_text)
 print(df.head())
